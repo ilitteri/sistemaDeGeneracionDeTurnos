cc42f08caf1736989662606fb7d2541c374a05bd
ilitteri <ilitteri@fi.uba.ar>
2021-02-20 20:41:25 -0300

    add comment

--- src/priority_queue.h
+++ src/priority_queue.h
@@ -5,10 +5,20 @@
 
 typedef cola_t PriorityQueue;
 
+/* Crea la estrucutra */
 PriorityQueue *priority_queue_create();
 
+/*  Destruye la estrucutura 
+*   Pre: la estructura fue creada.
+*/
 void priority_queue_destroy(PriorityQueue *queue, void (*destroy_patient) (Patient *patient));
 
+/*  Encola un paciente
+*   Pre: la estructura fue creada.
+*/
 bool enqueue_patient(PriorityQueue *queue, Patient *patient);
 
+/*  Desencola un paciente
+*   Pre: la estructura fue creada.
+*/
 Patient *dequeue_patient(PriorityQueue *queue);
