b60b821f47a45781625d43aa126ede1ea83124d3
ilitteri <ilitteri@fi.uba.ar>
2021-02-20 13:54:10 -0300

    added patient TDA implementation

---
+++ src/patient.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+#include "patient.h"
+
+typedef struct
+{
+    char *name;
+    size_t entry_year;
+} Patient;
+
+Patient *patient_check_in(char *name, size_t entry_year)
+{
+    Patient *patient;
+
+    if ((patient = malloc(sizeof(Patient))) == NULL)
+    {
+        return NULL;
+    }
+
+    patient->name = name;
+    patient->entry_year = entry_year;
+
+    return patient;
+}
+
+char *patient_name(const Patient *patient)
+{
+    return patient->name;
+}
+
+size_t patient_entry_year(const Patient *patient)
+{
+    return patient->entry_year;
+}
---
+++ src/patient.h
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+typedef struct Patient;
+
+/* Registra un paciente */
+Patient *patient_check_in(char *name, size_t entry_year);
+
+/*  Obtiene el nombre de el paciente (no modifica al paciente)
+*   Pre: el paciente fué registrado.
+*   Pos: nombre del paciente.
+*/
+char *patient_name(const Patient *patient);
+
+/*  Obtiene el anio de entrada del paciente (no modifica al paciente)
+*   Pre: el paciente fué registrado.
+*   Pos: anio de registro del paciente.
+*/
+size_t patient_entry_year(const Patient *patient);
