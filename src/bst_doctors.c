f427ef3e26e3d75f26cde6b5628605809970e084
FaustinoMillares <fmillares@fi.uba.ar>
2021-02-21 18:43:10 -0300

    Some casts

--- src/bst_doctors.c
+++ src/bst_doctors.c
@@ -17,12 +17,12 @@ BSTDoctors *bst_doctors_create(bst_key_cmp cmp, bst_doctor_destroy destroy_data)
 
 bool bst_doctors_save_doctor(BSTDoctors *doctors, const char *doctor_name, Doctor *doctor)
 {
-    return abb_guardar(doctors, doctor_name, doctor);
+    return abb_guardar(doctors, doctor_name, (void *)doctor);
 }
 
 Doctor *bst_doctors_get_doctor(const BSTDoctors *doctors, const char *doctor_name)
 {
-    return abb_obtener(doctors, doctor_name);
+    return (Doctor *)abb_obtener(doctors, doctor_name);
 }
 
 void bst_doctors_destroy(BSTDoctors *doctors)
